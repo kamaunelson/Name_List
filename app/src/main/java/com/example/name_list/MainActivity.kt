45e83836971a28b2bd9b1896e9949078b591a6f6
Nelson Kamau <nelson.kaguamere@strathmore.edu>
2024-01-13 23:53:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/name_list/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.name_list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.name_list.ui.theme.Name_ListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Name_ListTheme {
+                var name by remember{
+                    mutableStateOf("")
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    Row (
+                        modifier =  Modifier
+                            .fillMaxWidth()
+                    ){
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = {
+                                text -> name = text
+                            })
+                    }
+                }
+            }
+        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Name_ListTheme {
+//        Greeting("Android")
+//    }
+//}
