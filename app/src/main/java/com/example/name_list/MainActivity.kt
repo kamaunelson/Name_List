6f7be3ebb84e17de1371e9666f1f250e89fedc25
Nelson Kamau <nelson.kaguamere@strathmore.edu>
2024-01-14 00:24:48 +0300

    finished scripting the code now composing remains

--- app/src/main/java/com/example/name_list/MainActivity.kt
+++ app/src/main/java/com/example/name_list/MainActivity.kt
@@ -5,9 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -30,11 +36,16 @@ class MainActivity : ComponentActivity() {
                 var name by remember{
                     mutableStateOf("")
                 }
+                var names by remember{
+                    mutableStateOf(listOf<String>())
+                }
+                //body parent
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(16.dp)
                 ) {
+                    //child row
                     Row (
                         modifier =  Modifier
                             .fillMaxWidth()
@@ -43,7 +54,28 @@ class MainActivity : ComponentActivity() {
                             value = name,
                             onValueChange = {
                                 text -> name = text
-                            })
+                            },
+                            modifier = Modifier.weight(1f))
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            if (name.isNotBlank()){
+                                names = names +name
+                                name = ""
+                            }
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }
+                    //child column
+                    LazyColumn{
+                        items(names){currentName->
+                            Text(
+                                text = currentName,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp))
+                            Divider()
+                        }
                     }
                 }
             }
